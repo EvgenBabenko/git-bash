8d65c2997ab1667bd52b4238c12ecdde1571b3fa
EvgenyBabenko <evgen.babenko@gmail.com>
2025-06-02 15:48:16 +0300

    shell refactoring, responsive

--- src/Terminal/Shell/Shell.tsx
+++ src/Terminal/Shell/Shell.tsx
@@ -9,13 +9,10 @@ interface Props extends React.PropsWithChildren {
 }
 
 export const Shell = ({ children, path, terminalRef, userName }: Props) => {
-  const pathSegments = path.split("/").filter(Boolean).join("/");
-  const pathHeader = `MINGW64:/c/${userName}${
-    pathSegments.length > 0 ? `/${pathSegments}` : ""
-  }`;
+  const pathHeader = `MINGW64:/c/${userName}${path ? `/${path}` : ""}`;
 
   return (
-    <div className="text-left border border-white bg-black mx-auto text-sm">
+    <div className="text-left border border-white bg-black mx-auto text-sm w-full max-w-[600px]">
       <div className="bg-white p-[6px_4px] flex items-center justify-between text-black">
         <div className="flex items-center justify-center gap-x-1">
           <img src={logo} width="16" height="16" />
@@ -28,7 +25,7 @@ export const Shell = ({ children, path, terminalRef, userName }: Props) => {
         </div>
       </div>
       <div
-        className="overflow-y-auto h-[200px] p-1 text-sm font-[Roboto_Mono]"
+        className="overflow-y-auto h-[250px] p-1 text-sm font-[Roboto_Mono]"
         ref={terminalRef}
       >
         {children}
--- src/Terminal/ShellTitle/ShellTitle.tsx
+++ src/Terminal/ShellTitle/ShellTitle.tsx
@@ -4,17 +4,13 @@ interface Props {
 }
 
 export const ShellTitle = ({ path, userName }: Props) => {
-  const pathSegments = path.split("/").filter(Boolean).join("/");
-
   return (
     <span>
       <span style={{ color: "#00cc00" }}>
         {userName}@{window.navigator.platform}
       </span>{" "}
       <span style={{ color: "#cc00cc" }}>MINGW64</span>{" "}
-      <span style={{ color: "#cccc00" }}>
-        ~{pathSegments.length > 0 ? `/${pathSegments}` : ""}
-      </span>
+      <span style={{ color: "#cccc00" }}>~{path ? `/${path}` : ""}</span>
     </span>
   );
 };
