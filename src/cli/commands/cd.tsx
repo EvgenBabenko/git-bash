1aa12293f9000182dd915d9fc498a2da3074a895
EvgenyBabenko <evgen.babenko@gmail.com>
2025-06-18 22:35:02 +0300

    improve cd help

--- src/cli/commands/cd.tsx
+++ src/cli/commands/cd.tsx
@@ -1,26 +1,28 @@
 import { Command } from "../command-registry";
 import { emitter } from "../utils";
+import { extractValues } from "../utils/extract-values";
 
 export const cd: Command = {
   name: "cd",
   description: "Change the shell working directory.",
   help: (
-    <div>
-      <div>cd: cd [-P]</div>
-      <div>Options: </div>
-      <div>
-        -P Change to path P, if P is not specified, change to the root directory
-      </div>
-    </div>
+    <>
+      Usage: cd [DIR] <br />
+      Change the current directory to DIR. The default DIR is the value of the
+      HOME shell variable. <br />
+      `..' is processed by removing the immediately previous pathname component
+      back to a slash or the beginning of DIR. <br />
+      If DIR is not specified, change to the root directory
+    </>
   ),
-  run: ({ args: [P], cli }) => {
-    const path = P;
-    const rootPath = "";
+  run: ({ args, cli }) => {
+    const path = extractValues(args, [])[0];
 
     // Go to root if no path is provided
     if (!path) {
-      cli.path = rootPath;
+      cli.path = "";
       emitter.emit("PATH", cli.path);
+
       return "";
     }
 
@@ -35,6 +37,7 @@ export const cd: Command = {
       if (part === "..") {
         currentPathSegments.pop();
         currentChildren = cli.getChildren(currentPathSegments.join("/"));
+
         continue;
       }
 
